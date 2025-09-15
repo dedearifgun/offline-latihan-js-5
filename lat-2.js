8e282ffb54f7470c0277beb37bb29dfa83a715c0
dedearifgun <dedearifgunawan@gmail.com>
2025-09-15 09:40:40 +0700

    tugas map dan for each

--- lat-2.js
+++ lat-2.js
@@ -2,20 +2,23 @@ let huruf = ['a','b','c','d','e'];
 let hasil = "";
 
 // looping array
-huruf.forEach((item, index) => {
-    if (index === 0) {
-        // khusus index pertama
-        hasil += "aa\n";
-    } else {
-        // sisanya: huruf + "aba"
-        hasil += item + "aba\n";
-    }
+huruf.forEach((item) => {
+    hasil += item + "aba\n";
 });
 
 console.log(hasil);
 
 
+
 // menggunakan map
 let huruf1 = ['a','b','c','d','e'];
-let hasil1 = huruf1.map(huruf1 => huruf1+ 'ba')
+let hasil1 = huruf1.map(huruf1 => huruf1+ 'aba')
 console.log(hasil1);
+
+//menggunakan foreach
+let kata = ['a','b','c','d','e'];
+let hasil2 = "";
+kata.forEach((item) => {
+    hasil2 += item + "aba\n";
+console.log(hasil2);
+});
